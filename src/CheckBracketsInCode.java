850a77dc3adfbe59304c477309289ee095c482b8
Gabriel Gualteros <Roblox4@gmail.com>
2021-03-26 12:21:33 -0400

    First upload of brackets

---
+++ src/CheckBracketsInCode.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+class Bracket {
+    Bracket(char type, int position) {
+        this.type = type;
+        this.position = position;
+    }
+
+    boolean Match(char c) {
+        if (this.type == '[' && c == ']')
+            return true;
+        if (this.type == '{' && c == '}')
+            return true;
+        if (this.type == '(' && c == ')')
+            return true;
+        return false;
+    }
+
+    char type;
+    int position;
+}
+
+class check_brackets {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input_stream = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input_stream);
+        String text = reader.readLine();
+
+        	int eP = 0;
+        
+        Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        for (int position = 0; position < text.length(); ++position) {
+            char next = text.charAt(position);
+
+            if (next == '(' || next == '[' || next == '{') {
+                // Process opening bracket
+            	Bracket brack = new Bracket(next, position + 1);
+            	opening_brackets_stack.push(brack);
+            }
+
+            if (next == ')' || next == ']' || next == '}') {
+                // Process closing bracket
+            	if(opening_brackets_stack.isEmpty()) {
+            		eP = position + 1;
+            		break;
+            	}
+            	Bracket first = opening_brackets_stack.pop();
+            	if(!first.Match(next)) {
+            		eP = position + 1;
+            		break;
+            	}
+            }
+        }
+
+        // Printing answer, write your code here
+        if(eP == 0 && opening_brackets_stack.isEmpty()) {
+        	System.out.println("Good Match !");
+    }else {
+    	if(eP == 0) {
+    		while(opening_brackets_stack.size() > 1) {
+    			opening_brackets_stack.pop();
+    			eP = opening_brackets_stack.peek().position;
+    		}
+    	}
+    }
+        System.out.println(eP);
+    	
+    }
+}
